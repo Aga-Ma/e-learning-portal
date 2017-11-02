eeb104f80e1e11b3ff9a433f79486af3524fae64
Agnieszka <agnieszka.jureczko@o2.pl>
2017-11-02 23:34:44 +0100

    section 2 Forms

--- elearning/courses/models.py
+++ elearning/courses/models.py
@@ -1,5 +1,10 @@
+from django.core.urlresolvers import reverse
 from django.db import models
+
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
+
+    def get_absolute_url(self):
+        return reverse('course_detail', args=(self.id,))
 
--- elearning/elearning/urls.py
+++ elearning/elearning/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from courses.views import my_first_view
+from courses.views import course_detail, course_list, course_add
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^(?P<who>.*)/$', my_first_view)
+    url(r'^course_detail/(?P<course_id>\d)/$', course_detail,
+        name='course_detail'),
+    url(r'^course_add/$', course_add, name='course_add'),
+    url(r'^$', course_list),
 ]
