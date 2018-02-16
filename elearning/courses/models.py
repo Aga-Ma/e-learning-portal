15fb119b21f23f0dfe90ff8f07bb7bd2540ad261
Agnieszka <agnieszka.jureczko@o2.pl>
2018-02-16 14:21:24 +0100

    add models for courses sections, questions and answers

--- elearning/courses/models.py
+++ elearning/courses/models.py
@@ -13,3 +13,33 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Section(models.Model):
+    course = models.ForeignKey(Course)
+    title = models.CharField(max_length=100)
+    number = models.IntegerField()
+    test = models.TextField()
+
+    class Meta:
+        unique_together = ('course', 'number', )
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    section = models.ForeignKey(Section)
+    text = models.CharField(max_lenght=1000)
+
+    def __str__(self):
+        return self.text
+
+    
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    text = models.CharField(max_lenght=1000)
+    correct = models.BooleanField()
+
+    def __str__(self):
+        return self.text
