8b7821d491a4029c47d60d45fbfaee5197e69f1e
Agnieszka <agnieszka.jureczko@o2.pl>
2018-02-16 13:30:52 +0100

    add students to course model

--- elearning/courses/models.py
+++ elearning/courses/models.py
@@ -1,9 +1,12 @@
 from django.core.urlresolvers import reverse
 from django.db import models
 
+from students.models import User
+
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
+    students = models.ManyToManyField(User)
 
     def get_absolute_url(self):
         return reverse('course_detail', args=(self.id,))
