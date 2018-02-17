ce6b743a3028cf5ce5dd5e597813b4287c4c20ef
Agnieszka <agnieszka.jureczko@o2.pl>
2018-02-17 22:26:54 +0100

    adding get_all_scores_for_user method

--- elearning/students/views.py
+++ elearning/students/views.py
@@ -1,6 +1,19 @@
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 
+from courses.models import Course
+from courses.views import calculate_score
+
+
+def get_all_scores_for_user(user):
+    scores = []
+    for course in Course.objects.all():
+        course_scores = []
+        for section in course.section_set.order_by('number'):
+            course_scores.append((section, calculate_score(user, section),))
+        scores.append((course, course_scores),)
+    return scores
+
 
 def student_detail(request):
     if not request.user.is_authenticated():
@@ -8,4 +21,5 @@ def student_detail(request):
     student = request.user
     return render(request, 'students/student_detail.html', {
         'student': student,
+        'scores': get_all_scores_for_user(student),
     })
