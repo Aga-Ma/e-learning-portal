f79a102161595a052c16e425b724d49ce93726e4
Aga <agnieszkajureczko@o2.pl>
2020-02-23 16:01:34 +0100

    add heroku related settings

--- elearning/settings.py
+++ elearning/settings.py
@@ -115,12 +115,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'elearning', 'static')
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 
