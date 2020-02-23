b565ded17947b54e96c73d249d6f9ad7a936ec7d
Aga <agnieszkajureczko@o2.pl>
2020-02-23 18:44:38 +0100

    adding whitenoise middleware

--- elearning/settings.py
+++ elearning/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'elearning.urls'
